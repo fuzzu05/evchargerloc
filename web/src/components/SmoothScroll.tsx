124530fb2683ac89740513e14554d8124f505828
fuzzu05 <fuzailk05@gmail.com>
2026-08-18 05:28:41 +0530

    Remove all deprecated Lenis properties

--- web/src/components/SmoothScroll.tsx
+++ web/src/components/SmoothScroll.tsx
@@ -12,10 +12,6 @@ export default function SmoothScroll() {
     const lenis = new Lenis({
       duration: 1.8, // Slower, highly controlled speed so users can't surpass it wildly
       easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // Buttery smooth easing curve
-      mouseMultiplier: 1, // Controls how much the mouse wheel moves it
-      smoothTouch: false,
-      touchMultiplier: 2,
-      infinite: false,
     });
 
     let frameId: number;
